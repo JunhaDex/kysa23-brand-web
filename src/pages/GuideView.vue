d30fab378d407e230ca326f216ff85aaa5cc24c4
JunhaDex <dk.junha.dev@gmail.com>
2023-04-07 08:24:03 +0900

    add responsive

--- src/pages/GuideView.vue
+++ src/pages/GuideView.vue
@@ -1,11 +1,44 @@
 <template>
-    <main>Guide View</main>
+  <main>
+    <h1>Header Number One 111!</h1>
+    <h2>Header Number Two 222@</h2>
+    <h3>Header Number Three 333#</h3>
+    <p class="fonts">
+      <span class="xl">Font Size Tester</span><br />
+      <span class="lg">Font Size Tester</span><br />
+      <span class="md">Font Size Tester</span><br />
+      <span class="sm">Font Size Tester</span><br />
+      <span class="xs">Font Size Tester</span><br />
+    </p>
+  </main>
 </template>
 <script lang="ts" setup>
 import { onMounted } from 'vue';
 
 onMounted(() => {
-    console.log('Guide View');
+  console.log('Guide View');
 });
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.fonts {
+  .xl {
+    font-size: #{$font-xl};
+  }
+
+  .lg {
+    font-size: #{$font-lg};
+  }
+
+  .md {
+    font-size: #{$font-md};
+  }
+
+  .sm {
+    font-size: #{$font-sm};
+  }
+
+  .xs {
+    font-size: #{$font-xs};
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "./src/assets/sass/variables";
+        @import "./src/assets/sass/mixins";
+        `,
+      },
+    },
   },
   server: {
     port: 8080,
-    strictPort: true
-  }
-})
+    strictPort: true,
+  },
+});
